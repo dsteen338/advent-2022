05bace3892d71e4538db1697c9583c57bb7997df
Dylan Steen <dsteen338@gmail.com>
2022-12-02 05:13:56 +0000

    lint

--- 1-1.js
+++ 1-1.js
@@ -1,17 +1,22 @@
-const util = require('util');
-const fs = require('fs');
+const util = require("util");
+const fs = require("fs");
 const readFile = util.promisify(fs.readFile);
 
-const getCalories = async () => { 
-  const data = await readFile('input.txt', 'utf8');
-    return data.split('\n\n').map(line => line.split('\n').map(num => +num)).filter(line => !!line);
-}
+const getCalories = async () => {
+  const data = await readFile("input.txt", "utf8");
+  return data
+    .split("\n\n")
+    .map((line) => line.split("\n").map((num) => +num))
+    .filter((line) => !!line);
+};
 
 async function main() {
   const allCalories = await getCalories();
 
-  const summedCalories = allCalories.map((elvesCalories) => elvesCalories.reduce((acc, curr) => acc + curr));
-  
+  const summedCalories = allCalories.map((elvesCalories) =>
+    elvesCalories.reduce((acc, curr) => acc + curr)
+  );
+
   console.log(Math.max(...summedCalories));
 }
 
